e8974d8f2a5b074bc54cbe2ef706516ec1154645
unknown <thornhil@.ece.ualberta.ca>
2015-01-28 15:43:24 -0700

    added all error handling

--- Lab1/lab1.cpp
+++ Lab1/lab1.cpp
@@ -93,21 +93,14 @@ void UserMain(void * pd) {
      *
      */
 	// Initialize the semaphores we will use for ordered printing
-	if ((err = OSSemInit (&mySem, 1)) == OS_SEM_ERR){
-		display_error("Error initializing mySem", err);
-	}
 
-	if ((err = OSSemInit (&mySem2, 0)) == OS_SEM_ERR){
-			display_error("Error initializing mySem2", err);
-	}
+	display_error("Error initializing mySem", OSSemInit (&mySem, 1));
 
-	if ((err = OSSemInit (&mySem3, 0)) == OS_SEM_ERR){
-			display_error("Error initializing mySem3", err);
-	}
+	display_error("Error initializing mySem2", OSSemInit (&mySem2, 0));
 
-	if ((err = OSSemInit (&mySem4, 0)) == OS_SEM_ERR){
-			display_error("Error initializing mySem4", err);
-	}
+	display_error("Error initializing mySem3", OSSemInit (&mySem3, 0));
+
+	display_error("Error initializing mySem4", OSSemInit (&mySem4, 0));
 
 	#ifndef _DEBUG
     EnableSmartTraps();
@@ -155,7 +148,7 @@ void	Task1Main( void * pd) {
 	/* place semaphore usage code inside the loop */
 	while (1) {
 			//Wait for sem1
-			OSSemPend(&mySem, 0);
+			display_error("Error pending on mySem", OSSemPend(&mySem, 0));
 
 			//Position Cursor
 			myLCD.MoveCursor(LCD_LOWER_SCR, QUADRANT1);
@@ -168,10 +161,10 @@ void	Task1Main( void * pd) {
 
 			//Post to the appropriate sem based on dip switch
 			if (!getdipsw()){
-				OSSemPost(&mySem2);
+				display_error("Error posting to mySem2", OSSemPost(&mySem2));
 			} else {
 				myLCD.Clear(LCD_BOTH_SCR);
-				OSSemPost(&mySem4);
+				display_error("Error posting to mySem4", OSSemPost(&mySem4));
 			}
 	}
 
@@ -198,7 +191,7 @@ void StartTask2(void) {
 void	Task2Main( void * pd) {
 	/* place semaphore usage code inside the loop */
 	while (1) {
-			OSSemPend(&mySem2, 0);
+			display_error("Error pending on mySem2", OSSemPend(&mySem2, 0));
 
 			//Position the cursor
 			myLCD.MoveCursor(LCD_LOWER_SCR, QUADRANT2);
@@ -211,9 +204,9 @@ void	Task2Main( void * pd) {
 
 			//Post to the appropriate sem based on dip switch
 			if (!getdipsw()){
-				OSSemPost(&mySem3);
+				display_error("Error posting to mySem3", OSSemPost(&mySem3));
 			} else {
-				OSSemPost(&mySem);
+				display_error("Error posting to mySem", OSSemPost(&mySem));
 			}
 	}
 
@@ -241,7 +234,7 @@ void StartTask3(void) {
 void	Task3Main( void * pd) {
 	/* place semaphore usage code inside the loop */
 	while (1) {
-			OSSemPend(&mySem3, 0);
+		display_error("Error pending on mySem3", OSSemPend(&mySem3, 0));
 
 			//Position the cursor
 			myLCD.MoveCursor(LCD_UPPER_SCR, QUADRANT3);
@@ -254,9 +247,9 @@ void	Task3Main( void * pd) {
 
 			//Post to the appropriate sem based on dip switch
 			if (!getdipsw()){
-				OSSemPost(&mySem4);
+				display_error("Error posting to mySem4", OSSemPost(&mySem4));
 			} else {
-				OSSemPost(&mySem2);
+				display_error("Error posting to nySem2", OSSemPost(&mySem2));
 			}
 	}
 
@@ -284,7 +277,7 @@ void StartTask4(void) {
 void	Task4Main( void * pd) {
 	/* place semaphore usage code inside the loop */
 	while (1) {
-			OSSemPend(&mySem4, 0);
+		display_error("Error pending on mySem4", OSSemPend(&mySem4, 0));
 
 			//Position the cursor
 			myLCD.MoveCursor(LCD_UPPER_SCR, QUADRANT4);
@@ -298,9 +291,9 @@ void	Task4Main( void * pd) {
 			//Post to the appropriate sem based on dip switch
 			if (!getdipsw()){
 				myLCD.Clear(LCD_BOTH_SCR);
-				OSSemPost(&mySem);
+				display_error("Error posting to mySem", OSSemPost(&mySem));
 			} else {
-				OSSemPost(&mySem3);
+				display_error("Error posting to mySem3", OSSemPost(&mySem3));
 			}
 	}
 
