5b5827e8c03df67b3f1a418cc5b5ad8cecbfe296
unknown <thornhil@.ece.ualberta.ca>
2015-03-04 15:37:21 -0700

    Lab 3 demod

--- Lab3/lab3.cpp
+++ Lab3/lab3.cpp
@@ -25,12 +25,15 @@
 
 #define	WAIT_FOREVER 0
 #define IBUFF_SIZE_LINE 		40
-
+#define LCD_SECOND_LINE			40
+#define BUFFER_SIZE				50
 
 extern "C" {
 	void UserMain(void * pd);
 	void GraMain(void *);
+	void NetStatsMain(void *);
 	void StartGracefulStopTask(void);
+	void StartNetworStatisticsTask(void);
 	void InitLEDs(void);
 }
 
@@ -46,13 +49,13 @@ extern void ClearGracefulStop(void);
 */
 
 #define GRA_PRIO 				MAIN_PRIO + 1
-
+#define NETSTATS_PRIO 			MAIN_PRIO + 5
 
 asm( " .align 4 " );
 DWORD GraMainStk[USER_TASK_STK_SIZE] __attribute__( ( aligned( 4 ) ) );
+DWORD NetStatsSTK[USER_TASK_STK_SIZE] __attribute__( ( aligned( 4 ) ) );
 
-
-const char * AppName="Put your name here";
+const char * AppName="CH RT";
 
 Lcd myLCD;
 
@@ -98,6 +101,8 @@ void UserMain(void * pd) {
 
     myLCD.Clear(LCD_BOTH_SCR);
 
+    StartNetworStatisticsTask();
+
     while (1) {
     	OSTimeDly(TICKS_PER_SECOND*1);
 
@@ -134,3 +139,57 @@ void	GraMain( void * pd) {
 	}
 }
 
+/* Name: StartNetworStatisticsTask
+ * Description: Creates the Task responsible printing network
+ * usage statistics to the LCD screen.
+ * Inputs: none
+ * Outputs: none
+ */
+void StartNetworStatisticsTask(void) {
+	BYTE err = OS_NO_ERR;
+
+	/* start up the task that lights up LEDS */
+	err = display_error("StartNetworkStatisticsTask", OSTaskCreatewName(	NetStatsMain,
+					(void *)NULL,
+				 	(void *) &NetStatsSTK[USER_TASK_STK_SIZE],
+				 	(void *) &NetStatsSTK[0],
+				 	NETSTATS_PRIO, "NetStats Task" ));
+}
+
+/* Name: NetStatsMain
+ * Description: This task contains the network statistics printing code.
+ * Inputs:  void * pd -- pointer to generic data . Currently unused.
+ * Outputs: none
+ */
+void	NetStatsMain( void * pd) {
+	BYTE err = OS_NO_ERR;
+	while (1) {
+		vudword totalPackets = sim.fec_rmon_t.packets;
+		vudword broadcastPackets = sim.fec_rmon_t.bc_pkt;
+		vudword multicastPackets = sim.fec_rmon_t.mc_pkt;
+		vudword unicastPackets = totalPackets - broadcastPackets - multicastPackets;
+
+		char printBuff[BUFFER_SIZE];
+
+
+		myLCD.Clear(LCD_BOTH_SCR);
+		myLCD.Home(LCD_UPPER_SCR);
+
+		snprintf(printBuff, BUFFER_SIZE,"Total Packets: %lu ", totalPackets);
+		myLCD.PrintString(LCD_UPPER_SCR, printBuff);
+
+		myLCD.MoveCursor(LCD_UPPER_SCR, LCD_SECOND_LINE);
+		snprintf(printBuff, BUFFER_SIZE, "Broadcast Packets: %lu ", broadcastPackets);
+		myLCD.PrintString(LCD_UPPER_SCR, printBuff);
+
+		myLCD.Home(LCD_LOWER_SCR);
+		snprintf(printBuff, BUFFER_SIZE,"Multicast Packets: %lu ", multicastPackets);
+		myLCD.PrintString(LCD_LOWER_SCR, printBuff);
+
+		myLCD.MoveCursor(LCD_LOWER_SCR, LCD_SECOND_LINE);
+		snprintf(printBuff, BUFFER_SIZE, "Unicast Packets: %lu ", unicastPackets);
+		myLCD.PrintString(LCD_LOWER_SCR, printBuff);
+
+		OSTimeDly(TICKS_PER_SECOND*1);
+	}
+}
